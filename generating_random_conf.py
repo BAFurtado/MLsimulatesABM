d4167ca6cdae43b9157227e92cf3747b9759092f
B Furtado <furtadobb@gmail.com>
2017-12-02 17:52:04 -0200

    Minor fixes

--- generating_random_conf.py
+++ generating_random_conf.py
@@ -5,20 +5,20 @@ import pandas as pd
 import preparing_data
 
 d_uniform = {"HOUSE_VACANCY": [0.02, 0.4], "MARKUP": [0, .5], "MEMBERS_PER_FAMILY": [1, 5],
-             "PRODUCTION_MAGNITUDE": [1, 200], "SIZE_MARKET": [5, 50]}
+             "PRODUCTION_MAGNITUDE": [1, 200], "SIZE_MARKET": [5, 50], "PERCENTAGE_CHECK_NEW_LOCATION": [.01, .3]}
 d_taxes = {"TAX_CONSUMPTION": [.004, .00004], "TAX_ESTATE_TRANSACTION": [7e-06, 7e-08],
            "TAX_FIRM": [.004, .00004], "TAX_LABOR": [.0015, .000015], "TAX_PROPERTY": [4e-06, 4e-08],
            "TREASURE_INTO_SERVICES": [.8, 1.2]}
 d_bool = ["ALTERNATIVE0", "FPM_DISTRIBUTION", "WAGE_IGNORE_UNEMPLOYMENT"]
 d_perc = ["ALPHA", "BETA", "LABOR_MARKET", "PCT_DISTANCE_HIRING", "PERCENTAGE_ACTUAL_POP", "STICKY_PRICES"]
-d_acps = ["MANAUS", "BELEM", "MACAPA", "SAO LUIS", "TERESINA", "FORTALEZA", "JUAZEIRO DO NORTE - CRATO - BARBALHA",
-          "NATAL", "JOAO PESSOA", "CAMPINA GRANDE", "RECIFE", "PETROLINA - JUAZEIRO", "MACEIO", "ARACAJU",
-          "SALVADOR", "FEIRA DE SANTANA", "ILHEUS - ITABUNA", "BELO HORIZONTE", "JUIZ DE FORA", "IPATINGA",
-          "UBERLANDIA", "VITORIA", "VOLTA REDONDA - BARRA MANSA", "RIO DE JANEIRO", "CAMPOS DOS GOYTACAZES",
-          "SAO PAULO", "CAMPINAS", "SOROCABA", "SAO JOSE DO RIO PRETO", "SANTOS", "JUNDIAI",
-          "SAO JOSE DOS CAMPOS", "RIBEIRAO PRETO", "CURITIBA" "LONDRINA", "MARINGA", "JOINVILLE", "FLORIANOPOLIS",
-          "PORTO ALEGRE", "NOVO HAMBURGO - SAO LEOPOLDO", "CAXIAS DO SUL", "PELOTAS - RIO GRANDE",
-          "CAMPO GRANDE", "CUIABA", "GOIANIA", "BRASILIA"]
+d_acps = ['ARACAJU', 'BELEM', 'BELO HORIZONTE', 'BRASILIA', 'CAMPINA GRANDE', 'CAMPINAS', 'CAMPO GRANDE',
+          'CAMPOS DOS GOYTACAZES', 'CAXIAS DO SUL', 'CUIABA', 'CURITIBA', 'LONDRINA', 'FEIRA DE SANTANA',
+          'FLORIANOPOLIS', 'FORTALEZA', 'GOIANIA', 'ILHEUS - ITABUNA', 'IPATINGA', 'JOAO PESSOA', 'JOINVILLE',
+          'JUAZEIRO DO NORTE - CRATO - BARBALHA', 'JUIZ DE FORA', 'JUNDIAI', 'MACAPA', 'MACEIO', 'MANAUS',
+          'MARINGA', 'NATAL', 'NOVO HAMBURGO - SAO LEOPOLDO', 'PELOTAS - RIO GRANDE', 'PETROLINA - JUAZEIRO',
+          'PORTO ALEGRE', 'RECIFE', 'RIBEIRAO PRETO', 'RIO DE JANEIRO', 'SALVADOR', 'SANTOS', 'SAO JOSE DO RIO PRETO',
+          'SAO JOSE DOS CAMPOS', 'SAO LUIS', 'SAO PAULO', 'SOROCABA', 'TERESINA', 'UBERLANDIA', 'VITORIA',
+          'VOLTA REDONDA - BARRA MANSA']
 
 
 def generate(i=0):
@@ -28,14 +28,14 @@ def generate(i=0):
     for key in d_taxes.keys():
         data[key] = round(random.uniform(d_taxes[key][0], d_taxes[key][1]), 8)
     for each in d_bool:
-        data[each] = random.choice([True, False])
+        data[each] = random.choice(['True', 'False'])
     for each in d_perc:
         data[each] = round(random.random(), 2)
     data['PROCESSSING_ACPS'] = random.choice(d_acps)
     return pd.DataFrame(data, index=[i])
 
 
-def compound(n=250, df=pd.DataFrame()):
+def compound(n=500, df=pd.DataFrame()):
     for i in range(n):
         df = pd.concat([df, generate(i)])
     df = df.fillna(0)
--- running_classifier.py
+++ running_classifier.py
@@ -56,8 +56,11 @@ def main(x, y):
     m2 = run_random_forest_split(x, y)
     r = generating_random_conf.compound()
     descriptive_stats.print_conf_stats({'bases': [r], 'text':['generated']})
-    y2 = predict_random_forest_cross(m2, r[x.columns.tolist()])
-    descriptive_stats.print_conf_stats({'bases': [y2], 'text': ['sim2']})
+    print(x.columns)
+    print(r.columns)
+    # print(r[x.columns.tolist()])
+    # y2 = predict_random_forest_cross(m2, r[x.columns.tolist()])
+    # descriptive_stats.print_conf_stats({'bases': [y2], 'text': ['sim2']})
 
 
 if __name__ == "__main__":
