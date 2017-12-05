0acbc48d5d1949634ac53e628e7926deb52e02e4
furtadobb <bernardo.furtado@ipea.gov.br>
2017-12-05 16:41:59 -0200

    Fixed splitting sample. Changed to CSV saving

--- machines.py
+++ machines.py
@@ -13,29 +13,33 @@ from sklearn.utils import shuffle
 
 
 # Training classifiers (see note above)
-def run_classifiers(x, y):
+def run_classifiers(x, xt, y, yt):
+    models = ['Tree', 'KNeighbors', 'SVC', 'MPL', 'Voting']
+
     m1 = DecisionTreeClassifier(max_depth=7)
     m2 = KNeighborsClassifier(n_neighbors=3)
     m3 = SVC(C=1, kernel='poly', degree=3, probability=True)
     m4 = MLPClassifier(solver='lbfgs', early_stopping=True, activation='tanh')
-    voting = VotingClassifier(estimators=[('dt', m1), ('knn', m2), ('svc', m3), ('neural', m4)], voting='soft')
+    voting = VotingClassifier(estimators=[('dt', m1), ('knn', m2), ('svc', m3), ('neural', m4)],
+                              voting='soft')
     m1.fit(x, y)
     m2.fit(x, y)
     m3.fit(x, y)
     m4.fit(x, y)
     voting.fit(x, y)
-    for each in [m1, m2, m3, m4, voting]:
-        print('Score: {:.4f}'.format(each.score(x, y)))
+
+    cls = [m1, m2, m3, m4, voting]
+    for i in range(len(cls)):
+        print('Score {}: {:.4f}'.format(models[i], cls[i].score(xt, yt)))
     return m1, m2, m3, m4, voting
 
 
-def run_random_forest_split(x, y):
+def run_random_forest_split(x, xt, y, yt):
     x, y = shuffle(x, y)
     m1 = RandomForestClassifier(n_estimators=1000, criterion='entropy', bootstrap=True)
-    train_size = int(len(x) * .7)
-    m1.fit(x[train_size:], y['target'][train_size:])
-    accuracy = m1.score(x[:train_size:], y['target'][:train_size])
-    print('Accuracy Random Forest Manual data-splitting {:.4f}'.format(accuracy))
+    m1.fit(x, y)
+    accuracy = m1.score(xt, yt)
+    print('Accuracy Random Forest {:.4f}'.format(accuracy))
     return m1
 
 
@@ -48,5 +52,5 @@ if __name__ == "__main__":
     path = r'\\storage4\carga\MODELO DINAMICO DE SIMULACAO\Exits_python\JULY'
     target1 = 'gdp_index'
     target2 = 'gini_index'
-    a, b = main.get_data(path, target1, target2)
-    c, d, e, f, g = run_classifiers(a, b)
+    x, xt, y, yt = main.get_data(path, target1, target2)
+    c, d, e, f, g = run_classifiers(x, xt, y, yt)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import pickle
 
 import pandas as pd
 from numpy import set_printoptions
@@ -8,40 +7,43 @@ import descriptive_stats
 import generating_random_conf
 import machines
 import preparing_data
-
+from sklearn.model_selection import train_test_split
 set_printoptions(precision=4)
 
 
 def get_data(pathways, col1, col2):
-    name = 'pre_processed_data\\' + pathways[-4:]
+    name = 'pre_processed_data\\' + pathways[-4:] + '_x.csv'
     if os.path.exists(name):
-        with open(name, 'rb') as stored_data:
-            p = pickle.load(stored_data)
-            print('Loaded!')
-        return p[0], p[1]['target']
+        x = pd.read_csv(name, sep=';', index_col=False).drop('Unnamed: 0', axis=1)
+        y = pd.read_csv(name.replace('x', 'y'), sep=';', index_col=False)
+        print('Loaded!')
     else:
         x, y = preparing_data.main(pathways, col1, col2)
-        with open(name, 'wb') as stored_data:
-            pickle.dump([x, y], stored_data)
-            print('Saved!')
-        return x, y['target']
+        x.to_csv(name, sep=';')
+        y.to_csv(name.replace('x', 'y'), sep=';')
+        print('Saved!')
+    return train_test_split(x, y['target'], test_size=0.2, random_state=10)
 
 
-def print_results(conf, y):
-    res = pd.concat([conf, y], axis=1)
-    res = res.groupby(by=['t2']).agg('mean')
-    res.T.to_csv('outputs\\comparison.csv', sep=';')
+def print_results(x, y, x_r, y_r):
+    res = pd.concat([x, y], axis=1)
+    res = res.groupby(by=['target']).agg('mean')
+    res_r = pd.concat([x_r, y_r], axis=1)
+    res_r = res_r.groupby(by=['t2']).agg('mean')
+
+    final = pd.concat([res, res_r], axis=0)
+    final.T.to_csv('outputs\\comparison.csv', sep=';')
     print('\n Final results: \n')
-    print(res)
+    print(final)
 
 
-def main(x, y):
+def main(x, xt, y, yt):
     # Output basic descriptive stats for original dataset
     print('\n Training dataset summary: \n')
-    descriptive_stats.print_conf_stats({'bases': [x], 'text': ['actual']})
+    descriptive_stats.print_conf_stats({'bases': [pd.concat([x, xt], axis=0)], 'text': ['actual']})
 
     # Running model
-    m2 = machines.run_random_forest_split(x, y)
+    m2 = machines.run_random_forest_split(x, xt, y, yt)
 
     # Generating random configuration data to test against optimal results
     r = generating_random_conf.compound()
@@ -57,7 +59,7 @@ def main(x, y):
     y2 = pd.DataFrame({'t2': y2.tolist()})
 
     # Output results of machine on random data
-    print_results(r, y2)
+    print_results(pd.concat([x, xt], axis=0), pd.concat([y, yt], axis=0), r, y2)
 
 
 if __name__ == "__main__":
@@ -67,5 +69,5 @@ if __name__ == "__main__":
     path = r'\\storage4\carga\MODELO DINAMICO DE SIMULACAO\Exits_python\JULY'
     target1 = 'gdp_index'
     target2 = 'gini_index'
-    a, b = get_data(path, target1, target2)
-    main(a, b)
+    x_train, x_test, y_train, y_test = get_data(path, target1, target2)
+    main(x_train, x_test, y_train, y_test)
